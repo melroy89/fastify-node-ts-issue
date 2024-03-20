8d7056131421b06b2760b69fb9355526fbb70bcd
Melroy van den Berg <melroy@melroy.org>
2024-03-20 01:45:57 +0100

    Add some more comments

--- src/fastify.d.ts
+++ src/fastify.d.ts
@@ -2,14 +2,14 @@ import { MySQLPromisePool } from '@fastify/mysql'
 
 // Declare our version of the FastifyInstance
 declare module 'fastify' {
+
+  // Instance is already prepare for MySQL Promise Pool using Fastify-Mysql
+  // I also added some examples config when using Fastify-env
   interface FastifyInstance {
     config: {
       DATABASE_URL: string
       MAX_CONCURRENT: number
       MULTIPLE_STATATEMENTS: boolean
-      FALLBACK_LIMIT_RESULTS: number
-      PAGINATION_RESULTS_PER_PAGE: number
-      PAGINATION_LIMIT_PER_PAGE: number
     }
     mysql: MySQLPromisePool
   }
--- src/handlers/user.ts
+++ src/handlers/user.ts
@@ -2,10 +2,15 @@ import { FastifyPluginAsync } from 'fastify'
 import userRepository from '../repositories/userRepository.js'
 
 const userHandler: FastifyPluginAsync = async (instance) => {
+  // This additional repository layer is optional, you could also do your database queries in the handler
   const userRepo = userRepository(instance)
 
-  // Simple demo example (you should also define a schema for each API, which will make Fastify even faster)
+  // Simple demo example (you should also define a schema for each API, which will make Fastify even faster!!)
   instance.get('/', (req, reply) => {
+
+    // Get a user object
+    // Note: If you use a database call like MySQL Promose,
+    // be sure to make the this function async and use await, example: const [rows] = await userRepo.getUser()
     const obj = userRepo.getUser()
     reply.send(obj)
     // Try to sent an empty object, you will notice the payload plugin will kick in
